8cceee2995cf000b92b4ec6188413b5c93ff530b
Javorov1103 <k.javorov@gmail.com>
2017-04-21 15:44:02 +0300

    Project c#

---
+++ Project/Project/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using Project.Migrations;
+using Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Project
+{
+    public class MvcApplication :HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ProjectDbContext, Configuration>());
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ Project/Project/Models/ProjectDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Project.Models
+{
+   
+   
+
+    public class ProjectDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ProjectDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public virtual IDbSet<Article> Articles { get; set; }
+
+        public static ProjectDbContext Create()
+        {
+            return new ProjectDbContext();
+        }
+    }
+}
